8fde63b51c5d8b00d47cf1f47a1a21d91a9694ae
Matthew <matthew.guo@gmail.com>
2021-06-13 00:32:42 -0700

    main menu + levels 1-4 + win screen

---
+++ Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButton : MonoBehaviour {
+    public void ReturnToMainMenu() {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
